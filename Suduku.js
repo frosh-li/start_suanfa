98fb2f51a520a0bad688d3fcd8deaf02fbb0e3ea
lijunliang <junguoliang@163.com>
2018-09-14 15:38:47 +0800

    添加C++版本

--- Suduku.js
+++ Suduku.js
@@ -1,14 +1,14 @@
 const Sudoku = require('./libs/sudoku');
 const maps = require('./maps');
 
-// for(let map in maps) {
-//     let sdk = new Sudoku({
-//         display: false, // 是否打印过程，打印的速度慢很多
-//         sudokuMap: maps[map],
-//     }).start();
-// }
-let sdk = new Sudoku({
-    display: false, // 是否打印过程，打印的速度慢很多
-    sudokuMap: maps['hard1'],
-}).start();
+for(let map in maps) {
+    let sdk = new Sudoku({
+        display: false, // 是否打印过程，打印的速度慢很多
+        sudokuMap: maps[map],
+    }).start();
+}
+// let sdk = new Sudoku({
+//     display: false, // 是否打印过程，打印的速度慢很多
+//     sudokuMap: maps['hard1'],
+// }).start();
 //console.log(sdk.sudokuMap);
